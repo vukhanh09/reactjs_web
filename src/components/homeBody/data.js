b504d2ab61d4ab178140773ee6ffb5141a0a380f
vukhanh09 <vukhanh0920@gmail.com>
2021-11-13 12:51:30 +0700

    add middle section

--- src/components/homeBody/data.js
+++ src/components/homeBody/data.js
@@ -16,4 +16,39 @@ export const subData = [
     }
 ]
 
+export const middleData = [
+    {
+        id:0,
+        topic:'Thế giới',
+        title:'Iran diễn tập tại khu vực hơn một triệu km2',
+        src:'https://i1-kinhdoanh.vnecdn.net/2021/11/12/11-11-2021-07-44-11-tan3779-co-8548-6679-1636694451.jpg?w=120&h=72&q=100&dpr=2&fit=crop&s=4uZuDktCE1fc1ZfCjrIxvg',
+        description:'Quân đội Iran bắt đầu diễn tập thường niên tại khu vực ven eo biển Hormuz, khi sắp diễn ra đàm phán về thỏa thuận hạt nhân với phương Tây vào cuối tháng.',
+        extend_description:'Diễn tập Zolfaghar-1400 bắt đầu ngày 7/11 nhằm "nâng cao khả năng sẵn sàng đối đầu với ...'
+    },
+    {
+        id:1,
+        topic:'Thời sự',
+        title:'Chủ tịch Quốc hội: Gói kích thích không chỉ để phục hồi kinh tế mà còn phát triển xã hội',
+        src:'https://i1-kinhdoanh.vnecdn.net/2021/11/12/11-11-2021-07-44-11-tan3779-co-8548-6679-1636694451.jpg?w=120&h=72&q=100&dpr=2&fit=crop&s=4uZuDktCE1fc1ZfCjrIxvg',
+        description:'Chủ tịch Quốc hội Vương Đình Huệ lưu ý khi xây dựng chương trình khôi phục kinh tế cần chú trọng cả tổng cung lẫn tổng cầu, tức cả việc phục hồi, phát triển xã hội.',
+        extend_description:'Các bang đệ đơn kiện chính quyền Tổng thống Mỹ gồm Missouri, Nebraska, Arkansas, Kansas ...'
+    },
+    {
+        id:2,
+        topic:'Thế giới',
+        title:'10 bang kiện chính quyền Biden',
+        src:'https://i1-vnexpress.vnecdn.net/2021/11/12/anhcat20210726T190838Z15276005-8804-1693-1636706474.jpg?w=120&h=72&q=100&dpr=2&fit=crop&s=qNFX-qionM8rMpN9wz6fgA',
+        description:'Tổng chưởng lý 10 bang Mỹ kiện chính quyền Tổng thống Joe Biden lên tòa liên bang vì quy định bắt buộc nhân viên y tế tiêm vaccine Covid-19.',
+        extend_description:'Các bang đệ đơn kiện chính quyền Tổng thống Mỹ gồm Missouri, Nebraska, Arkansas, Kansas ...'
+    },
+    {
+        id:3,
+        topic:'Kinh doanh',
+        title:'Chưa điều chỉnh mục tiêu bội chi giai đoạn 2021-2025',
+        src:'https://i1-kinhdoanh.vnecdn.net/2021/11/12/dsc251515895142041589514231706-2560-7091-1636707835.jpg?w=680&h=408&q=100&dpr=1&fit=crop&s=6RrThAhUWIHv4PSWVXjmkQ',
+        description:'Ôtô chở bê tông nhựa tổng tải trọng 44 tấn qua cầu cho phép xe dưới 5 tấn, khiến nhịp cầu bị sập, giao thông tê liệt nhiều giờ.',
+        extend_description:'Khi Taliban áp sát thủ đô Kabul hôm 15/8, đội ...'
+    }
+]
+
 

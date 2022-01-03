3b6964fc94859d1bbc78633af7934b001b9bc693
vukhanh09 <vukhanh0920@gmail.com>
2022-01-03 16:42:00 +0700

    add interface admin

--- src/components/ItemAdmin.js
+++ src/components/ItemAdmin.js
@@ -1,17 +1,19 @@
 
 import styles from './CSS/ItemAdmin.module.css'
+import { Link } from 'react-router-dom'
 function ItemAdmin({title,url}){
+    
     return (
         <div className={styles.container}>
             <img src={url} className={styles.image} alt='search'/>
             <div className={styles.component}>
                 <p className={styles.title}>{title}</p>
                 <div className={styles.optionArea}>
-                    <p className={styles.option}>Chi tiết</p>
+                    <Link to = '/admin/view-post/hello' className={styles.linkPost}>
+                        <p className={styles.option}>Chi tiết</p>
+                    </Link>
                     <p className={styles.option}>Xóa</p>
                 </div>
-
-
             </div>
         </div>
     )

9d94659e3810da8d18b213f9ceeeff4f1ad2f65d
vukhanh09 <vukhanh0920@gmail.com>
2022-01-10 01:31:59 +0700

    add api for admin search news

--- src/components/ItemAdmin.js
+++ src/components/ItemAdmin.js
@@ -1,8 +1,33 @@
 
 import styles from './CSS/ItemAdmin.module.css'
 import { Link } from 'react-router-dom'
-function ItemAdmin({title,url_image,url}){
-    
+import Cookies from 'js-cookie';
+import axiosConfig from '../config/axiosConfig';
+
+function ItemAdmin({title,url_image,url,news_id,deleteNews}){
+    const token = Cookies.get('access_token_admin')
+
+    const deletePost = ()=>{
+        axiosConfig({
+            method:'delete',
+            url:'/news/delete-news',
+            headers: {"Authorization" : `Bearer ${token}`},
+            data:{
+                news_id:news_id
+            }
+        })
+        .then(res=>{
+            console.log(res)
+            if(res.data.code===200){
+                alert('Delete post successfully!')
+                deleteNews(news_id)
+            }
+        })
+        .catch(err=>{
+            alert(err)
+        })
+
+    }
     return (
         <div className={styles.container}>
             <img src={url_image} className={styles.image} alt='search'/>
@@ -12,7 +37,7 @@ function ItemAdmin({title,url_image,url}){
                     <Link to = {`/admin/view-post/${url}`} className={styles.linkPost}>
                         <p className={styles.option}>Chi tiết</p>
                     </Link>
-                    <p className={styles.option}>Xóa</p>
+                    <p className={styles.option} onClick={deletePost} >Xóa</p>
                 </div>
             </div>
         </div>
--- src/screens/ManagePost.js
+++ src/screens/ManagePost.js
@@ -11,57 +11,56 @@ function ManagePost(){
     const title = 'Best Thanksgiving Leftovers Sandwich'
     const [searchTitle,setTitle] = useState('')
     const [listNews,setListNews] = useState([])
+    const [topicRender,setTopicRender] = useState('')
+    const [optionRender,setOptionRender] = useState('')
+    const dictTopic = {'thoisu':'Thời sự','gocnhin':'Góc nhìn','thegioi':'Thế giới','kinhdoanh':'Kinh doanh','dulich':'Du lịch','none':''}
 
     useEffect(()=>{
-        axiosConfig.get('/news/get-all-news')
-        .then(res=>{
-            // var lsNews = res.data.data.listNews
-            setListNews(res.data.data.listNews)
-        })
-        .catch(err=>{
-            console.log(err)
-        })
+        // console.log(optionRender)
+        if(topicRender === '' && searchTitle === '')
+            axiosConfig.get('/news/get-all-news')
+            .then(res=>{
+                // var lsNews = res.data.data.listNews
+                setListNews(res.data.data.listNews)
+            })
+            .catch(err=>{
+                console.log(err)
+                setListNews([])
+            })
+        else {
+            console.log(topicRender)
+            axiosConfig.get('/news/get-news-by-topic',{
+                params:{
+                    topic: topicRender,
+                    title:searchTitle
+                }
+            })
+            .then(res=>{
+                // var lsNews = res.data.data.listNews
+                setListNews(res.data.data.listNews)
+            })
+            .catch(err=>{
+                setListNews([])
+                console.log(err)
+            })
 
-    },[])
+        }
+
+    },[optionRender])
+
+
+    const deleteNews = (id)=>{
+        const newList = listNews.filter(item=>item.news_id!=id)
+        setListNews(newList)
+    }
     
-    const middleData = [
-        {
-            id:0,
-            topic:'Thế giới',
-            title:'Iran diễn tập tại khu vực hơn một triệu km2',
-            src:'https://i1-kinhdoanh.vnecdn.net/2021/11/12/11-11-2021-07-44-11-tan3779-co-8548-6679-1636694451.jpg?w=120&h=72&q=100&dpr=2&fit=crop&s=4uZuDktCE1fc1ZfCjrIxvg',
-            description:'Quân đội Iran bắt đầu diễn tập thường niên tại khu vực ven eo biển Hormuz, khi sắp diễn ra đàm phán về thỏa thuận hạt nhân với phương Tây vào cuối tháng.',
-            extend_description:'Diễn tập Zolfaghar-1400 bắt đầu ngày 7/11 nhằm "nâng cao khả năng sẵn sàng đối đầu với ...'
-        },
-        {
-            id:1,
-            topic:'Thời sự',
-            title:'Chủ tịch Quốc hội: Gói kích thích không chỉ để phục hồi kinh tế mà còn phát triển xã hội',
-            src:'https://i1-kinhdoanh.vnecdn.net/2021/11/12/11-11-2021-07-44-11-tan3779-co-8548-6679-1636694451.jpg?w=120&h=72&q=100&dpr=2&fit=crop&s=4uZuDktCE1fc1ZfCjrIxvg',
-            description:'Chủ tịch Quốc hội Vương Đình Huệ lưu ý khi xây dựng chương trình khôi phục kinh tế cần chú trọng cả tổng cung lẫn tổng cầu, tức cả việc phục hồi, phát triển xã hội.',
-            extend_description:'Các bang đệ đơn kiện chính quyền Tổng thống Mỹ gồm Missouri, Nebraska, Arkansas, Kansas ...'
-        },
-        {
-            id:2,
-            topic:'Thế giới',
-            title:'10 bang kiện chính quyền Biden',
-            src:'https://i1-vnexpress.vnecdn.net/2021/11/12/anhcat20210726T190838Z15276005-8804-1693-1636706474.jpg?w=120&h=72&q=100&dpr=2&fit=crop&s=qNFX-qionM8rMpN9wz6fgA',
-            description:'Tổng chưởng lý 10 bang Mỹ kiện chính quyền Tổng thống Joe Biden lên tòa liên bang vì quy định bắt buộc nhân viên y tế tiêm vaccine Covid-19.',
-            extend_description:'Các bang đệ đơn kiện chính quyền Tổng thống Mỹ gồm Missouri, Nebraska, Arkansas, Kansas ...'
-        },
-        {
-            id:4,
-            topic:'Thể thao',
-            title:'Chủ tịch PSG mong Mbappe tiếp bước Messi',
-            src:'https://i1-thethao.vnecdn.net/2021/11/30/Untitled-5836-1638238730.jpg?w=120&h=72&q=100&dpr=1&fit=crop&s=8h7r6xxbg1EBwv48hr9N-A',
-            description:'Nasser Al-Khelaifi hy vọng Kylian Mbappe sẽ giành Quả Bóng Vàng khi khoác áo PSG như Lionel Messi.',
-            extend_description:'Khi Taliban áp sát thủ đô Kabul hôm 15/8, đội ...'
-        }]
     
-
-
+    
+    
     const handleSearch = ()=>{
-        // console.log(document.getElementById("topics").value)
+        let topic = dictTopic[document.getElementById("topics").value]
+        setTopicRender(topic)
+        setOptionRender(topic+searchTitle)
     }
 
     return (
@@ -96,16 +95,9 @@ function ManagePost(){
                 <div className={styles.parentPost}>
                     {
                         listNews.map((item,id) =>{
-                            return <ItemAdmin key={id} url_image = {item.url_image[0]} title={item.title} url={item.url} />
+                            return <ItemAdmin key={id} url_image = {item.url_image[0]} title={item.title} url={item.url} news_id = {item.news_id} deleteNews={deleteNews}/>
                         })
                     }
-
-                    {/* <ItemAdmin url = {url} title={title} />
-                    <ItemAdmin url = {url} title={title} />
-                    <ItemAdmin url = {url} title={title} />
-                    <ItemAdmin url = {url} title={title} />
-                    <ItemAdmin url = {url} title={title} /> */}
-
                 </div>
     
             </div>

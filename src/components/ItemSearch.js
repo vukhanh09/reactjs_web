03c40731d7ec862377cd13e1f5ac6c48926baace
Tuan Nguyen <tuantoquq@gmail.com>
2022-02-08 01:31:39 +0700

    final merge API

--- src/components/ItemSearch.js
+++ src/components/ItemSearch.js
@@ -3,12 +3,25 @@
 import styles from './CSS/ItemSearch.module.css'
 import clsx from "clsx";
 import rmImg from "../assets/remove.png"
+import { removeNewsFromWatchLater } from '../api/watchLaterApi';
+import Cookies from 'js-cookie';
 
 function ItemSearch({data,op1,isViewLater = 0}){
     const classes = clsx({
         [styles.container_border]:op1,
         [styles.container_no_border] :!op1,
     })
+
+    const removeFromWatchLater = () => {
+        isViewLater = 0;
+        removeNewsFromWatchLater(Cookies.get("access_token"), data?.news_id)
+        .then(res => {
+            console.log(res)
+            window.location.reload();
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
 
     return(
         <div className={classes}>
@@ -23,7 +36,7 @@ function ItemSearch({data,op1,isViewLater = 0}){
                     isViewLater === 1 && 
                     <div className={styles.utilElement}>
                         <h3 className={styles.title}><a href={`/view-post/${data?.url}`}>{data.title}</a></h3>
-                        <img src={rmImg} className={styles.rmButton}/>
+                        <img src={rmImg} className={styles.rmButton} onClick={removeFromWatchLater}/>
                     </div>
 
                 }
